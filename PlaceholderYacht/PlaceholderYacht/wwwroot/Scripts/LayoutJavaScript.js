449ee9496bdf74310801061dfa3637a0f769e172
Alex k <alekra76@kth.se>
2017-12-04 10:40:04 +0100

    First upload

---
+++ PlaceholderYacht/PlaceholderYacht/wwwroot/Scripts/LayoutJavaScript.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    $(window).bind("scroll", function () {
+        var distance = 1;
+        if ($(window).scrollTop() > distance) {
+            $(".Nav-Index").addClass("scrolled");
+        }
+        else {
+            $(".Nav-Index").removeClass("scrolled");
+        }
+    });
+
+
+    $(".menu li").click(function () {
+        if (!$(this).hasClass("active")) {
+            $(".menu li.active").removeClass("active");
+            var pageurl = "/home/" + window.location.href.substr(window.location.href
+                .lastIndexOf("/") + 1);
+            console.log(pageurl)
+            $("#nav ul li a").each(function () {
+                if ($(this).attr("href") == pageurl || $(this).attr("href") == '')
+                    $(this).addClass("active");
+            })
+
+        }
+    });
+
+    var path = window.location.pathname.split("/").pop();
+
+    // Account for home page with empty path
+    if (path == '') {
+        path = 'index';
+    }
+
+    var target = $('#nav a[href="/home/' + path + '"]');
+    // Add active class to target link
+    var s = $('.menu li[class="' + path + '"]');
+    s.addClass('active');
+});
