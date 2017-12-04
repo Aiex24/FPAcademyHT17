c2e10af21221544df75d7b307237ff1f3d092af4
tobiasrondin <31711293+tobiasrondin@users.noreply.github.com>
2017-12-04 12:38:16 +0100

    Fiiplar med Google maps-api:t

--- PlaceholderYacht/PlaceholderYacht/wwwroot/Scripts/MapIntegration.js
+++ PlaceholderYacht/PlaceholderYacht/wwwroot/Scripts/MapIntegration.js
@@ -1,1 +1,10 @@
 ﻿//Google Maps API-key: AIzaSyA9rdRh5jniAD0TYjDIRhSqQP5ZLf6p5P4
+var map;
+function initiateMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+    
+}
+
