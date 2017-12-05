0c44085d7e68f18ead74e1da37186c37de48f2e3
matlij <mattiaslij@gmail.com>
2017-12-05 10:13:12 +0100

    Mattias Tisdag morgon

--- PlaceholderYacht/PlaceholderYacht/wwwroot/Scripts/MapIntegration.js
+++ PlaceholderYacht/PlaceholderYacht/wwwroot/Scripts/MapIntegration.js
@@ -3,9 +3,12 @@
 var map;
 var lineSymbol;
 var bounds;
-
+var clickCounter = 0;
+var clickStart = [];
+var clickEnd = [];
 
 function initiateMap() {
+
     map = new google.maps.Map(document.getElementById('Map'), {
         center: { lat: 59.2, lng: 19.1 },
         zoom: 9,
@@ -13,20 +16,19 @@ function initiateMap() {
         zoomControl: false,
         streetViewControl: false
     });
+    //Rita pilar när kartan är laddad
+    google.maps.event.addListener(map, 'tilesloaded', function (event) {
+        bounds = map.getBounds().toJSON();
+        lineSymbol = { path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW };
+    });
+
     google.maps.event.addListener(map, 'click', function (event) {
         var lat = event.latLng.lat();
         var lng = event.latLng.lng();
-        
+
+        drawLine(lat, lng);
         calcWindSpeed(lat, lng);
     });
-    //Rita pilar när kartan är laddad
-    google.maps.event.addListener(map, 'tilesloaded', function (event) {
-        bounds = map.getBounds().toJSON();     
-        lineSymbol = { path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW };
-        
-
-    });
-
 };
 
 function calcWindSpeed(lat, lng) {
@@ -42,6 +44,8 @@ function calcWindSpeed(lat, lng) {
             let windSpeed = result.wind.speed;
             let windDegree = result.wind.deg;
             let windDirection;
+
+            //TODO, snygga till med for-loop
             if ((windDegree > 337.5 && windDegree <= 360) && (windDegree => 0 && windDegree <= 22.5)) { windDirection = 'N'; }
             else if (windDegree > 22.5 && windDegree <= 67.5) { windDirection = 'NE'; }
             else if (windDegree > 67.5 && windDegree <= 112.5) { windDirection = 'E'; }
@@ -51,7 +55,6 @@ function calcWindSpeed(lat, lng) {
             else if (windDegree > 247.5 && windDegree <= 292.5) { windDirection = 'W'; }
             else if (windDegree > 292.5 && windDegree <= 337.5) { windDirection = 'NW'; }
 
-            alert("Windspeed: " + windSpeed + " Degrees: " + windDegree + " Direction: " + windDirection);
             //Testdata - det här ska ritas på ett annat ställe och vara generellt...
             drawArrow(bounds.north - ((bounds.north - bounds.south) / 2), bounds.east - ((bounds.east - bounds.west) / 2), 'NE');
         }
@@ -73,8 +76,35 @@ function drawArrow(latOrigin, longOrigin, windDirection) {
         }],
         map: map
     });
-
 }
 
+function drawLine(lat, lng) {
 
+    if (clickCounter % 2 == 0) {
+        clickStart[0] = lat;
+        clickStart[1] = lng;
+    }
+    else if (clickCounter % 2 == 1) {
+        clickEnd[0] = lat;
+        clickEnd[1] = lng;
+    }
+    clickCounter++;
 
+    if (clickStart.length > 0 && clickEnd.length > 0) {
+        var lineSymbol = {
+            path: 'M 0,-1 0,1',
+            strokeOpacity: 1,
+            scale: 4
+        };
+        var line = new google.maps.Polyline({
+            path: [{ lat: clickStart[0], lng: clickStart[1] }, { lat: clickEnd[0], lng: clickEnd[1] }],
+            strokeOpacity: 0,
+            icons: [{
+                icon: lineSymbol,
+                offset: '0',
+                repeat: '20px'
+            }],
+            map: map
+        });
+    }
+}
