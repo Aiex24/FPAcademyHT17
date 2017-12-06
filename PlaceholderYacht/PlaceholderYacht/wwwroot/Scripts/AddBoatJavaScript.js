c7c2188ba28df0d3d6952568059df3e328f972f1
tobiasrondin <31711293+tobiasrondin@users.noreply.github.com>
2017-12-06 17:12:38 +0100

    Javascript nu igen! Mer Knappar & sånt!!!

--- PlaceholderYacht/PlaceholderYacht/wwwroot/Scripts/AddBoatJavaScript.js
+++ PlaceholderYacht/PlaceholderYacht/wwwroot/Scripts/AddBoatJavaScript.js
@@ -2,17 +2,27 @@
 
 $(document).ready(function () {
     let rowId = 1;
-    let firstHTML = "";
+    let pureHTMLRow = "";
+    //Plockar ut de element vi vill ha in i den nya raden
     let htmlOfFirst = document.querySelectorAll(".RowToSelect");
-    htmlOfFirst.forEach((node) => firstHTML += node.outerHTML);
-    console.log(htmlOfFirst);
-    console.log(firstHTML);
+    //smackar in hela kalaset i en enda sträng!
+    htmlOfFirst.forEach((node) => pureHTMLRow += node.outerHTML);
 
     $("#AddRowButton").click(
-        function () {
-            let htmlRow = firstHTML.replace(/0/g, rowId);            rowId++;
-            //console.log(htmlRow);
+        () => {
+            let htmlRow = pureHTMLRow.replace(/0/g, rowId);            rowId++;
             $("#ButtonRow").before(htmlRow);
         }
     );
+
+    $("#VppInputTable").on("click", ".RemoveRowButton", (e) => {
+        let thisButton = e.target;
+        let row = thisButton.dataset.row;
+        console.log(row);
+
+        let followingSiblings = $(thisButton).parent().parent().nextAll(".RowToSelect");
+        console.log(followingSiblings);
+        //console.log(e.target.dataset.row);
+
+    });
 });
