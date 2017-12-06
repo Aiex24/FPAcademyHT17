96fc62675ba249ad209fa71136eb4b32343c86c5
matlij <mattiaslij@gmail.com>
2017-12-06 14:27:15 +0100

    Mattias Login added

--- PlaceholderYacht/PlaceholderYacht/Controllers/AccountController.cs
+++ PlaceholderYacht/PlaceholderYacht/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using PlaceholderYacht.Models.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -33,11 +34,28 @@ namespace PlaceholderYacht.Controllers
         {
             return RedirectToAction("Index", "Home");
         }
-        
+
         [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AccountLoginVM viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var result = await signInManager.PasswordSignInAsync(viewModel.UserName, viewModel.Password, false, false);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(nameof(AccountLoginVM.UserName), "Wrong username or password");
+                return View();
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
