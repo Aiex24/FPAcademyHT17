534471577536f09f3755bd3420fb8946ac789e53
tobiasrondin <31711293+tobiasrondin@users.noreply.github.com>
2017-12-11 11:42:16 +0100

    validering

--- PlaceholderYacht/PlaceholderYacht/wwwroot/Scripts/AddBoatJavaScript.js
+++ PlaceholderYacht/PlaceholderYacht/wwwroot/Scripts/AddBoatJavaScript.js
@@ -9,7 +9,7 @@
     //Plockar ut input elementen i första raden och sätter deras värde till en tom sträng
     $(elementOfFirstRow).find(".colUserInput").each((index, element) => {
         $(element).attr('value', '');
-    })
+    });
 
     //Smackar in första radens HTML kod i en enda sträng!
     let pureHTMLRow = $(elementOfFirstRow).prop('outerHTML');
@@ -19,13 +19,14 @@
             let htmlRow = pureHTMLRow.replace(/0/g, rowId);
             $("#ButtonRow").before(htmlRow);
             rowId++;
+            validateBoatForm();
         }
     );
     //TODO: Putsa Regexen så den inte ändrar några tabellvärden.
     $("#VppInputTable").on("click", ".RemoveRowButton", (e) => {
         let thisButton = e.target;
         let thisButtonRowNumber = thisButton.dataset.row;
-        let thisRow = $(`tr[data-row = "${thisButtonRowNumber}"]`)
+        let thisRow = $(`tr[data-row = "${thisButtonRowNumber}"]`);
         let followingSiblings = thisRow.last().nextAll(".RowToSelect");
         //tar bort den aktuella raden (och valideringsraden)
         thisRow.remove();
@@ -37,6 +38,17 @@
             trElement.outerHTML = trElement.outerHTML.replace(regExVar, rowReplacing);
         });
         rowId--;
-
+        validateBoatForm();
     });
+
+    function validateBoatForm() {
+        //Tar bort och lägger till unobtrusive validering på samtliga fält i formen för att alla element ska lira.
+        var form = $("#boatForm");
+        $(form).removeData('validator');
+        $(form).removeData('unobtrusiveValidation');
+        $.validator.unobtrusive.parse(form);
+            //.removeData("validator") /* added by the raw jquery.validate plugin */
+            //.removeData("unobtrusiveValidation");  /* added by the jquery unobtrusive plugin*/
+        //$.validator.unobtrusive.parse(form);
+    }
 });
