16e819138eaf36329772a2ba580aeeff657102db
MikaelAngelstam <mikael.angelstam@gmail.com>
2017-12-12 14:24:26 +0100

    Ignore

--- PlaceholderYacht/PlaceholderYacht/Models/BoatDevRepository.cs
+++ PlaceholderYacht/PlaceholderYacht/Models/BoatDevRepository.cs
@@ -173,5 +173,10 @@ namespace PlaceholderYacht.Models
         {
             throw new NotImplementedException();
         }
+
+        public double[] CalcDistance(double latitude1, double longitude1, double latitude2, double longitude2, string unit, string method, int minAngle)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
